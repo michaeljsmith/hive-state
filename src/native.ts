b70df38d205069ae666f7aeaf6affbe5b91702c1
Michael Smith <msmith@msmith.id.au>
2021-08-19 10:43:12 +1000

    Fix typing of native.

--- src/native.ts
+++ src/native.ts
@@ -1,11 +1,11 @@
 import { ArgumentId, asArgumentId, InstanceNode, newNodeId, NodeId } from "./block/index.js";
 import { NativeFunctor, ScalarType } from "./scalar/index.js";
-import { addNode, currentScope } from "./scope.js";
-import { brandAsValue, Value } from "./value.js";
+import { addNode } from "./scope.js";
+import { Value } from "./value.js";
 
 export function native<Args extends unknown[], T> (
     fn: (...args: Args) => T)
-: (...args: Value<ScalarType<Args>>[]) => Value<ScalarType<T>> {
+: (...args: {[K in keyof Args]: Value<ScalarType<Args[K]>>}) => Value<ScalarType<T>> {
   return (...args) => {
     // Create a new node to represent the mapping.
     const nodeId = newNodeId();
